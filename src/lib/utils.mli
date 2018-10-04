f848bf15af42476dde2cdc8a81b306481a53131a
Romain Beauxis <toots@rastageeks.org>
2018-10-04 11:10:14 -0500

    Add printexc to Utils.

--- src/lib/utils.mli
+++ src/lib/utils.mli
@@ -6,6 +6,7 @@ val partition : int -> 'a array -> 'a array array
 val escape : string -> string
 val replace_process : string -> unit Callback.t
 val delete : Js.Dict.key -> 'a Js.Dict.t -> unit
+val printexc : exn -> string
 
 module Json : sig
   val parse : string -> 'a Js.t
