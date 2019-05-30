e7e02c27dda749936dcc305b7df50d09ee5cbe0c
Romain Beauxis <toots@rastageeks.org>
2019-05-30 15:17:13 +0200

    Cleanup doc.

--- src/lib/instances.mli
+++ src/lib/instances.mli
@@ -6,7 +6,7 @@ open BsAsyncMonad
 (** Module to register new instances. *)
 module Runtime : sig
   (** Configuration for an intance whose callback is
-    * executed when receiving a message. *) 
+      executed when receiving a message. *) 
   type subscription_instance = {
     topic:        string;
     subscription: string;
@@ -14,18 +14,18 @@ module Runtime : sig
   } [@@bs.deriving abstract]
 
   (** Instances can be triggered either by running a simple callback
-    * or by receiving a [pubsub] message. *)
+      or by receiving a [pubsub] message. *)
   type t = [
     | `Callback of unit -> unit Callback.t
     | `Subscription of subscription_instance
   ]
 
   (** Register a new instance. Must be called at runtime to have 
-    * this instance available. *) 
+      this instance available. *) 
   val register : string -> t -> unit
 
   (** Setup all instances. Used internally to register all required
-    * [pubsub] topics and subscriptions. *) 
+      [pubsub] topics and subscriptions. *) 
   val setup : unit -> unit Callback.t
 
   (** Run instance. Used internally at runtime. *)
